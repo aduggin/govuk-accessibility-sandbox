b9899c7bde006b196a83322ffe6853c99db28313
Tom Byers <tombaromba@gmail.com>
2015-10-28 20:29:58 +0000

    Add JavaScripts for autocomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,4 +9,18 @@ $(document).ready(function() {
     toggleContent.showHideRadioToggledContent();
     toggleContent.showHideCheckboxToggledContent();
 
+    $('.country-autocomplete').each(function (idx, elm) {
+      GOVUK.autocompletes.add($(elm));
+    });
+
+    // Custom events
+
+    // Bind all autocomplete events
+    $.each(['initialized', 'opened', 'closed', 'movedto', 'updated'], function (idx, evt) {
+      $(document).bind('typeahead:' + evt, function () {
+        var autocompleteEvent = GOVUK.autocompletes.createEvent(evt);
+
+        autocompleteEvent.trigger.apply(GOVUK.autocompletes, arguments);
+      });
+    });
 });
