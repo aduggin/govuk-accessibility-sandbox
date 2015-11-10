b5f0864c38d84c78f65cf87424b8b4a035495edf
Alistair Duggin <alistair.duggin@btinternet.com>
2015-11-10 09:07:24 +0000

    Make autocomplete dependencies more obvious

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,19 +8,4 @@ $(document).ready(function() {
     var toggleContent = new ShowHideContent();
     toggleContent.showHideRadioToggledContent();
     toggleContent.showHideCheckboxToggledContent();
-
-    $('.country-autocomplete').each(function (idx, elm) {
-      GOVUK.autocompletes.add($(elm));
-    });
-
-    // Custom events
-
-    // Bind all autocomplete events
-    $.each(['initialized', 'opened', 'closed', 'movedto', 'updated'], function (idx, evt) {
-      $(document).bind('typeahead:' + evt, function () {
-        var autocompleteEvent = GOVUK.autocompletes.createEvent(evt);
-
-        autocompleteEvent.trigger.apply(GOVUK.autocompletes, arguments);
-      });
-    });
 });
